df250cc3d2c7e8fc2754aa3c6e156af52a3566fe
adkhey1 <hans.jauch123@gmail.com>
2023-01-19 15:35:44 +0100

    Insert Online State

--- js/app.js
+++ js/app.js
@@ -434,3 +434,19 @@ if (info) {
     }
     updateNetworkInfo(info);
 }
+
+
+document.getElementById('status1').innerHTML = navigator.onLine ? 'online' : 'offline';
+
+var target = document.getElementById('target1');
+
+function handleStateChange() {
+    var timeBadge = new Date().toTimeString().split(' ')[0];
+    var newState = document.createElement('p');
+    var state = navigator.onLine ? 'online' : 'offline';
+    newState.innerHTML = '' + timeBadge + ' Connection state changed to ' + state + '.';
+    target.appendChild(newState);
+}
+
+window.addEventListener('online', handleStateChange);
+window.addEventListener('offline', handleStateChange);
