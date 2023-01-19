7e8f6dfe0c4beb96363b2715fe9d7b30fb747d09
adkhey1 <hans.jauch123@gmail.com>
2023-01-19 15:31:52 +0100

    Insert Network Type & Speed

--- js/app.js
+++ js/app.js
@@ -413,3 +413,24 @@ if ('permissions' in navigator) {
         navigator.getUserMedia({audio: true}, noop, noop)
     }
 }
+
+
+
+function getConnection() {
+    return navigator.connection || navigator.mozConnection ||
+        navigator.webkitConnection || navigator.msConnection;
+}
+
+function updateNetworkInfo(info) {
+    document.getElementById('networkType').innerHTML = info.type;
+    document.getElementById('effectiveNetworkType').innerHTML = info.effectiveType;
+    document.getElementById('downlinkMax').innerHTML = info.downlinkMax;
+}
+
+var info = getConnection();
+if (info) {
+    info.onchange = function (event) {
+        updateNetworkInfo(event.target);
+    }
+    updateNetworkInfo(info);
+}
